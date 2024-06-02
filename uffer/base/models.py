b53b2762ad2a9f162fa57e2f342826fcd8fc6438
Alexsander Soares <alexsandersoarest@gmail.com>
2024-06-01 21:50:34 -0300

    fix: OneToOne relationship no has on_delete property

--- uffer/base/models.py
+++ uffer/base/models.py
@@ -7,7 +7,8 @@ class Usuario (models.Model):
     nome = models.CharField(max_length=256, validators=[validate_slug])
     email = models.CharField(max_length=320, validators=[validate_email])
     created = models.DateTimeField(auto_now_add=True)
+    foto = models.ImageField
 
 class Motorista(models.Model):
     cnh = models.BigIntegerField(unique=True)
-    usuario_id = models.OneToOneField(to=Usuario)
+    usuario_id = models.OneToOneField(to=Usuario, on_delete=models.DO_NOTHING)
